87dbb87c81e1cb0ed848665292546c93cfaeb252
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-05-22 23:59:18 +0100

    Initial project setup

---
+++ src/main/java/com/spring/microservices/payment/creditcard/statemachine/PaymentCreditcardStatemachineApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.payment.creditcard.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCreditcardStatemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentCreditcardStatemachineApplication.class, args);
+	}
+
+}
